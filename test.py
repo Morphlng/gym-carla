64f60f3c1453fd058066348553f22031465a9441
Morphlng <1193115470@qq.com>
2022-11-01 14:12:47 +0800

    fixed "sensor out of scoped" issue

--- test.py
+++ test.py
@@ -5,51 +5,88 @@
 # This work is licensed under the terms of the MIT license.
 # For a copy, see <https://opensource.org/licenses/MIT>.
 
+from time import sleep
 import gym
 import gym_carla
 import carla
+import subprocess
+import os
+import atexit
+import signal
+
 
 def main():
-  # parameters for the gym_carla environment
-  params = {
-    'number_of_vehicles': 100,
-    'number_of_walkers': 0,
-    'display_size': 256,  # screen size of bird-eye render
-    'max_past_step': 1,  # the number of past steps to draw
-    'dt': 0.1,  # time interval between two frames
-    'discrete': False,  # whether to use discrete control space
-    'discrete_acc': [-3.0, 0.0, 3.0],  # discrete value of accelerations
-    'discrete_steer': [-0.2, 0.0, 0.2],  # discrete value of steering angles
-    'continuous_accel_range': [-3.0, 3.0],  # continuous acceleration range
-    'continuous_steer_range': [-0.3, 0.3],  # continuous steering angle range
-    'ego_vehicle_filter': 'vehicle.lincoln*',  # filter for defining ego vehicle
-    'port': 2000,  # connection port
-    'town': 'Town03',  # which town to simulate
-    'task_mode': 'random',  # mode of the task, [random, roundabout (only for Town03)]
-    'max_time_episode': 1000,  # maximum timesteps per episode
-    'max_waypt': 12,  # maximum number of waypoints
-    'obs_range': 32,  # observation range (meter)
-    'lidar_bin': 0.125,  # bin size of lidar sensor (meter)
-    'd_behind': 12,  # distance behind the ego vehicle (meter)
-    'out_lane_thres': 2.0,  # threshold for out of lane
-    'desired_speed': 8,  # desired speed (m/s)
-    'max_ego_spawn_times': 200,  # maximum times to spawn ego vehicle
-    'display_route': True,  # whether to render the desired route
-    'pixor_size': 64,  # size of the pixor labels
-    'pixor': False,  # whether to output PIXOR observation
-  }
+    # parameters for the gym_carla environment
+    params = {
+        'number_of_vehicles': 100,
+        'number_of_walkers': 0,
+        'display_size': 256,  # screen size of bird-eye render
+        'max_past_step': 1,  # the number of past steps to draw
+        'dt': 0.1,  # time interval between two frames
+        'discrete': False,  # whether to use discrete control space
+        'discrete_acc': [-3.0, 0.0, 3.0],  # discrete value of accelerations
+        # discrete value of steering angles
+        'discrete_steer': [-0.2, 0.0, 0.2],
+        'continuous_accel_range': [-3.0, 3.0],  # continuous acceleration range
+        # continuous steering angle range
+        'continuous_steer_range': [-0.3, 0.3],
+        'ego_vehicle_filter': 'vehicle.lincoln*',  # filter for defining ego vehicle
+        'port': 2000,  # connection port
+        'town': 'Town03',  # which town to simulate
+        # mode of the task, [random, roundabout (only for Town03)]
+        'task_mode': 'random',
+        'max_time_episode': 1000,  # maximum timesteps per episode
+        'max_waypt': 12,  # maximum number of waypoints
+        'obs_range': 32,  # observation range (meter)
+        'lidar_bin': 0.125,  # bin size of lidar sensor (meter)
+        'd_behind': 12,  # distance behind the ego vehicle (meter)
+        'out_lane_thres': 2.0,  # threshold for out of lane
+        'desired_speed': 8,  # desired speed (m/s)
+        'max_ego_spawn_times': 200,  # maximum times to spawn ego vehicle
+        'display_route': True,  # whether to render the desired route
+        'pixor_size': 64,  # size of the pixor labels
+        'pixor': False,  # whether to output PIXOR observation
+    }
 
-  # Set gym-carla environment
-  env = gym.make('carla-v0', params=params)
-  obs = env.reset()
+    # Set gym-carla environment
+    env = gym.make('carla-v0', params=params)
+    obs = env.reset()
 
-  while True:
-    action = [2.0, 0.0]
-    obs,r,done,info = env.step(action)
+    while True:
+        action = [2.0, 0.0]
+        obs, r, done, info = env.step(action)
 
-    if done:
-      obs = env.reset()
+        if done:
+            obs = env.reset()
 
 
 if __name__ == '__main__':
-  main()
+    server = subprocess.Popen(
+        [
+            os.environ.get("CARLA_SERVER"),
+            "-RenderOffScreen",
+            "-fps=20",
+            "-carla-server",
+            "-carla-rpc-port={}".format(2000),
+            "-carla-streaming-port=0"
+        ],
+        stdout=None,
+        stderr=subprocess.STDOUT,
+        preexec_fn=os.setsid,
+    )
+
+    pgid = os.getpgid(server.pid)
+
+    def on_exit():
+        print("on_exit")
+        os.killpg(pgid, signal.SIGKILL)
+
+    # register a cleanup function
+    atexit.register(on_exit)
+    signal.signal(signal.SIGINT, on_exit)
+    signal.signal(signal.SIGTSTP, on_exit)
+
+    # wait for server fully started
+    sleep(5)
+
+    main()
